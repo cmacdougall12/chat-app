b1ebc9ef6d9b05a217305766525b31522cc1d159
cmacdougall12 <cammacdougall12@gmail.com>
2021-09-23 15:36:15 -0600

    completed app

---
+++ client/containers/Rooms.tsx
@@ -0,0 +1,56 @@
+import { useRef } from "react";
+import { useSockets } from "../context/socket.context";
+import EVENTS from "../config/events";
+import styles from "../styles/Room.module.css";
+
+const RoomsContainer = () => {
+  const { socket, roomId, rooms } = useSockets();
+  const newRoomRef = useRef(null);
+
+  function handleCreateRoom() {
+    //get the room name
+    const roomName = newRoomRef.current.value || "";
+
+    if (!String(roomName).trim()) return;
+
+    //emit room created event
+    socket.emit(EVENTS.CLIENT.CREATE_ROOM, { roomName });
+
+    //set room to empty string
+    newRoomRef.current.value = "";
+  }
+
+  function handleJoinRoom(key) {
+    if (key === roomId) return;
+
+    socket.emit(EVENTS.CLIENT.JOIN_ROOM, key);
+  }
+
+  return (
+    <nav className={styles.wrapper}>
+      <div className={styles.createRoomWrapper}>
+        <input ref={newRoomRef} placeholder="Room Name" />
+        <button className="cta" onClick={handleCreateRoom}>
+          Create Room
+        </button>
+      </div>
+      <ul className={styles.roomList}>
+        {Object.keys(rooms).map((key) => {
+          return (
+            <div key={key}>
+              <button
+                disabled={key === roomId}
+                title={`Join ${rooms[key].name}`}
+                onClick={() => handleJoinRoom(key)}
+              >
+                {rooms[key].name}
+              </button>
+            </div>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default RoomsContainer;
